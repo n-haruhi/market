2e18676010dcfca8c8a6e363708357b5503d41ad
n-haruhi <hr23nakanishi@gmail.com>
2024-04-06 03:59:01 +0000

    [Update]admin/homes,items作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,12 @@
 Rails.application.routes.draw do
+  namespace :admin do
+    get 'items/index'
+    get 'items/new'
+    get 'items/show'
+    get 'items/edit'
+  end
+  namespace :admin do
+    get 'homes/top'
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
