59e2a943d77d8d015b0afc058fcdfe077f54be52
n-haruhi <hr23nakanishi@gmail.com>
2024-04-07 05:46:46 +0000

    [Update]admin/genres,orders,order_details作成

--- config/routes.rb
+++ config/routes.rb
@@ -4,14 +4,16 @@ Rails.application.routes.draw do
 
     resources :items, only: [:index, :show]
     resources :addresses, only: [:index, :create, :edit, :update, :destroy]
-
   end
 
   namespace :admin do
     get 'top' => 'homes#top', as: 'top'
 
     resources :items, only: [:index, :new, :create, :show, :edit, :update, :destroy]
-
+    resources :genres, only: [:index, :create, :edit, :update, :destroy]
+    resources :orders, only: [:show, :update] do
+      resources :order_details, only: [:update]
+    end
   end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
